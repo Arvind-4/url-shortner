757c64178b0f61645c39dfc91c9e8de6548a3d84
Arvind A <pingarvindforquestions@gmail.com>
2024-11-18 14:03:31 +0530

    fix: for rest_framework rewrite

--- shortner/views.py
+++ shortner/views.py
@@ -1,27 +1,36 @@
-from django.http import JsonResponse, HttpResponseRedirect, HttpResponse, HttpRequest
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
-from .models import URL
-from urllib.parse import urlparse
 from collections import Counter
-from shortner.validators import validate_url
+from rest_framework import status
+from urllib.parse import urlparse
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404, redirect
 
-import json
+from shortner.models import URL
+from shortner.serializers import URLSerializer
 
-@csrf_exempt
-def shorten_url(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
-        url = json.loads(request.body).get("url")
-        new_url = validate_url(url)
+
+class ShortenURLView(APIView):
+    serializer_class = URLSerializer
+    def post(self, request: Request):
+        serializer = URLSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        new_url = serializer.validated_data["url"]
         obj, _ = URL.objects.get_or_create(url=new_url)
-        return JsonResponse({"shortened_url": f"/{obj.shortcode}"})
+        return Response({"shortened_url": f"/{obj.shortcode}"}, status=status.HTTP_201_CREATED)
 
-def redirect_to_original(request: HttpRequest, path: str) -> HttpResponseRedirect:
-    obj = get_object_or_404(URL, shortcode=path)
-    return HttpResponseRedirect(obj.url)
 
-def get_metrics(request: HttpRequest) -> JsonResponse:
-    objs = URL.objects.all()
-    domain_counter = Counter(urlparse(obj.url).netloc for obj in objs)
-    top_domains = domain_counter.most_common(3)
-    return JsonResponse(dict(top_domains))
+class RedirectToOriginalView(APIView):
+    serializer_class = None
+    def get(self, request: Request, path: str):
+        obj = get_object_or_404(URL, shortcode=path)
+        return redirect(obj.url)
+
+
+class MetricsView(APIView):
+    serializer_class = None
+    def get(self, request: Request):
+        objs = URL.objects.all()
+        domain_counter = Counter(urlparse(obj.url).netloc for obj in objs)
+        top_domains = domain_counter.most_common(3)
+        return Response(dict(top_domains), status=status.HTTP_200_OK)
