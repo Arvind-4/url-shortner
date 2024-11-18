69b817eaa217b17df8c77c42ffe328a363eecf85
Arvind A <pingarvindforquestions@gmail.com>
2024-11-18 09:20:21 +0530

    fix: validate the url before the creation

--- shortner/views.py
+++ shortner/views.py
@@ -1,25 +1,23 @@
-from django.http import JsonResponse, HttpResponseRedirect, HttpResponse, HttpRequest, Http404
+from django.http import JsonResponse, HttpResponseRedirect, HttpResponse, HttpRequest
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from .models import URL
 from urllib.parse import urlparse
 from collections import Counter
+from shortner.validators import validate_url
 
 import json
 
 @csrf_exempt
 def shorten_url(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
-        new_url = json.loads(request.body).get("url")
-        print("new_url", new_url)
+        url = json.loads(request.body).get("url")
+        new_url = validate_url(url)
         obj, _ = URL.objects.get_or_create(url=new_url)
         return JsonResponse({"shortened_url": f"/{obj.shortcode}"})
 
 def redirect_to_original(request: HttpRequest, path: str) -> HttpResponseRedirect:
-    qs = URL.objects.filter(shortcode__iexact=path)
-    if qs.count() != 1 and not qs.exists():
-        raise Http404
-    obj = qs.first()
+    obj = get_object_or_404(URL, shortcode=path)
     return HttpResponseRedirect(obj.url)
 
 def get_metrics(request: HttpRequest) -> JsonResponse:
