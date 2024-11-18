c93021cc7107d2a74e6aef0bb10bad820bd7d7e4
Arvind A <pingarvindforquestions@gmail.com>
2024-11-18 10:16:56 +0530

    fix(static): static root in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-STATIC_ROOT = BASE_DIR / "static-cdn"
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
